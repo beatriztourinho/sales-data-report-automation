a11b978270ab66e50328ff1724cdce2370af3b1f
Beatriz Tourinho <btourinhon@gmail.com>
2022-07-27 11:40:43 -0300

    extract sales data

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import pyautogui
 import pyperclip
 import time
+import pandas as pd
 
 pyautogui.PAUSE = 1
 
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
-
 pyperclip.copy('https://drive.google.com/drive/folders/1uileaevywoNZO5QtbEJyW8DDpcLk3SA7?usp=sharing')
 pyautogui.hotkey('ctrl', 'v')
 pyautogui.press('enter')
 
-pyautogui.hotkey('win', 'up')
+
 time.sleep(5)
+
 pyautogui.click(x=397, y=400)
 pyautogui.click(x=1165, y=189)
 pyautogui.click(x=987, y=526)
-time.sleep(5)
+
+time.sleep(7)
+
+tabela = pd.read_excel(r"C:\Users\Beatriz\Downloads\Vendas - Dez.xlsx")
+
+quantidade = tabela["Quantidade"].sum()
+faturamento = tabela["Valor Final"].sum()
