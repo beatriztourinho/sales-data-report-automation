b10db0b2215ae52edc0c6cf8d813094bc7637aba
Beatriz Tourinho <btourinhon@gmail.com>
2022-07-27 10:51:17 -0300

    open google drive

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ pyautogui.PAUSE = 1
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
+
+pyperclip.copy('https://drive.google.com/drive/folders/1uileaevywoNZO5QtbEJyW8DDpcLk3SA7?usp=sharing')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
